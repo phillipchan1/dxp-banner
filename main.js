56d0a539992b4c06baad39287447722e14f073f7
Phillip Chan <phillip.chan@liferay.com>
2017-03-15 16:07:29 -0700

    Successfully refactored to allow different contexts per shape set

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ var generateNumBetween = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var canvas = document.getElementById('dxp-background');
-var ctx = canvas.getContext('2d');
+// var canvas = document.getElementById('dxp-background');
+// var ctx = canvas.getContext('2d');
 
 var draw = function() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
 	
 	for (let p = 0; p < shapeCreator.sets.length; p++) {
+		var currentSet = shapeCreator.sets[p];
+
+		currentSet.context.clearRect(0, 0, currentSet.canvas.width, currentSet.canvas.height);
 		var currentSet = shapeCreator.sets[p];
 
 		for (var i = 0; i < currentSet.shapes.length; i++) {
@@ -22,17 +25,20 @@ var draw = function() {
 var Shape = function(opts) {
 	var self = this;
 
-	this.shape = opts.shape;
-	this.speed = opts.speed;
-	this.size = opts.size;
-	this.xPos = opts.xPos;
-	this.yPos = opts.yPos;
+	this.context = opts.context;
+	this.canvas = opts.canvas;
+
 	this.opacity = 0.05 + Math.random() * 0.5;
 	this.originalY = opts.yPos;
+	this.shape = opts.shape;
+	this.size = opts.size;
+	this.speed = opts.speed;
+	this.xPos = opts.xPos;
+	this.yPos = opts.yPos;
 
 	var shapeAttributes = {
 		circle: function() {
-			ctx.arc(
+			self.context.arc(
 				self.xPos,
 				self.yPos,
 				self.size,
@@ -42,7 +48,7 @@ var Shape = function(opts) {
 			);
 		},
 		square: function() {
-			ctx.rect(
+			self.context.rect(
 				self.xPos, 
 				self.yPos, 
 				self.size, 
@@ -56,12 +62,14 @@ var Shape = function(opts) {
 	}
 
 	this.update = function() {
+		var ctx = self.context;
+
 		ctx.beginPath();
 
 		this.drawShape(this.shape);
 
 		// move horizontally
-		if (this.xPos < canvas.width + this.size) {
+		if (this.xPos < self.canvas.width + this.size) {
 			this.xPos = this.xPos + (0.3 * this.speed);	
 		} else {
 			this.xPos = -this.size;
@@ -69,7 +77,7 @@ var Shape = function(opts) {
 		}
 
 		// move vertically
-		if (this.yPos > (canvas.height / 2)) {
+		if (this.yPos > (self.canvas.height / 2)) {
 			this.yPos = this.yPos - 0.1;
 		} else {
 			this.yPos = this.yPos + 0.1;
@@ -95,6 +103,8 @@ var shapeCreator = {
 		var set = {};
 
 		set.shapes = [];
+		set.context = opts.context;
+		set.canvas = opts.canvas;
 
 		if (!opts.shape) return;
 
@@ -104,17 +114,19 @@ var shapeCreator = {
 			var maxSpeed = opts.maxSpeed || 5;
 			var startingX = opts.startingX || 5;
 
-			var randomX = generateNumBetween(-200, canvas.width);
-			var randomY = generateNumBetween(0, canvas.height);
+			var randomX = generateNumBetween(-200, opts.canvas.width);
+			var randomY = generateNumBetween(0, opts.canvas.height);
 			var speed = generateNumBetween(minSpeed, maxSpeed);
 			var size = generateNumBetween(0, opts.maxSize);
 
 			var shape = new Shape({
+				canvas: opts.canvas,
+				context: opts.context,
 				shape: opts.shape,
-				xPos: randomX,
-				yPos: randomY,
 				size: size,
-				speed: speed
+				speed: speed,
+				xPos: randomX,
+				yPos: randomY
 			})
 
 			set.shapes.push(shape);
@@ -142,9 +154,13 @@ var init = function() {
 		maxSize: 10
 	});
 
+	var canvas = document.getElementById('dxp-background2');
+	var ctx = canvas.getContext('2d');
 
 	shapeCreator.add({
-		// shape: 'square',
+		canvas: canvas,
+		context: ctx,
+		shape: 'square',
 		num: 10,
 		maxSpeed: 5,
 		maxSize: 10
