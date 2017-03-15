8f75dfdcd7cbdf608a08612a9eabbd4abcb30985
Phillip Chan <phillip.chan@liferay.com>
2017-03-15 16:56:03 -0700

    Define different movements and shapes

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ var generateNumBetween = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-// var canvas = document.getElementById('dxp-background');
-// var ctx = canvas.getContext('2d');
-
 var draw = function() {
-	
 	
 	for (let p = 0; p < shapeCreator.sets.length; p++) {
 		var currentSet = shapeCreator.sets[p];
@@ -27,37 +23,132 @@ var Shape = function(opts) {
 
 	this.context = opts.context;
 	this.canvas = opts.canvas;
-
+	this.movement = opts.movement;
 	this.opacity = 0.05 + Math.random() * 0.5;
-	this.originalY = opts.yPos;
+	this.originalY;
+	this.originalX;
 	this.shape = opts.shape;
 	this.size = opts.size;
 	this.speed = opts.speed;
+	this.style = opts.style;
 	this.xPos = opts.xPos;
 	this.yPos = opts.yPos;
 
-	var shapeAttributes = {
-		circle: function() {
-			self.context.arc(
-				self.xPos,
-				self.yPos,
-				self.size,
-				0,
-				Math.PI * 2,
-				false
-			);
+	this.movements = {
+		converge: {
+			starting: function() {
+				var xPos = generateNumBetween(-200, opts.canvas.width);
+				var yPos = generateNumBetween(0, opts.canvas.height);
+
+				self.originalY = yPos
+				self.originalX = xPos
+				self.xPos = xPos
+				self.yPos = yPos;
+			},
+			move: function() {
+				// move horizontally
+				if (self.xPos < self.canvas.width + self.size) {
+					self.xPos = self.xPos + (0.3 * self.speed);	
+				} else {
+					self.xPos = -self.size;
+					self.yPos = self.originalY;
+				}
+
+				// move vertically
+				if (self.yPos > (self.canvas.height / 2)) {
+					self.yPos = self.yPos - 0.1;
+				} else {
+					self.yPos = self.yPos + 0.1;
+				}
+			}
 		},
-		square: function() {
-			self.context.rect(
-				self.xPos, 
-				self.yPos, 
-				self.size, 
-				self.size
-			)
+		expand: {
+			starting: function() {
+				var xPos = generateNumBetween(-200, opts.canvas.width / 2);
+				var yPos = generateNumBetween(
+					// 40% from top
+					(opts.canvas.height / 2) - (opts.canvas.height * .1), 
+
+					// 60% from top
+					(opts.canvas.height / 2) + (opts.canvas.height * .1)
+				);
+
+				self.originalY = yPos
+				self.originalX = xPos
+				self.xPos = xPos
+				self.yPos = yPos;
+			},
+			move: function() {
+				// move horizontally
+				if (self.xPos < self.canvas.width + self.size) {
+					self.xPos = self.xPos + (0.3 * self.speed);	
+				} else {
+					self.xPos = -self.size;
+					self.yPos = self.originalY;
+				}
+
+				// move vertically
+				if (self.yPos > (self.canvas.height / 2)) {
+					self.yPos = self.yPos + 0.2;
+				} else {
+					self.yPos = self.yPos - 0.2;
+				}
+			}
+		}
+	}
+
+	this.move = function(movement) {
+		this.movements[movement].move();
+	};
+
+	this.styles = {
+		solid: function() {
+			// style
+			self.context.fillStyle = 'rgba(185, 211, 238,' + self.opacity + ')';
+			self.context.fill();
+		},
+		outline: function() {
+			self.context.strokeStyle = 'rgba(185, 211, 238,' + self.opacity + ')';
+			self.context.stroke();
 		}
 	};
 
+	this.setStyle = function() {
+		this.styles[this.style]();
+	};
+
+	// set starting position
+	this.movements[this.movement].starting();
+	
 	this.drawShape = function(shape) {
+		var shapeAttributes = {
+			circle: function() {
+				self.context.arc(
+					self.xPos,
+					self.yPos,
+					self.size,
+					0,
+					Math.PI * 2,
+					false
+				);
+			},
+			square: function() {
+				self.context.rect(
+					self.xPos, 
+					self.yPos, 
+					self.size, 
+					self.size
+				)
+			},
+			rectangle: function() {
+				self.context.rect(
+					self.xPos, 
+					self.yPos, 
+					self.size * 2, 
+					self.size
+				)
+			}
+		};
 		shapeAttributes[shape]();
 	}
 
@@ -66,33 +157,23 @@ var Shape = function(opts) {
 
 		ctx.beginPath();
 
+		// draw shape
 		this.drawShape(this.shape);
 
-		// move horizontally
-		if (this.xPos < self.canvas.width + this.size) {
-			this.xPos = this.xPos + (0.3 * this.speed);	
-		} else {
-			this.xPos = -this.size;
-			this.yPos = this.originalY;
-		}
+		// define movement
+		this.move(self.movement);
 
-		// move vertically
-		if (this.yPos > (self.canvas.height / 2)) {
-			this.yPos = this.yPos - 0.1;
-		} else {
-			this.yPos = this.yPos + 0.1;
-		}
+		// define style
+		this.setStyle(self.style)
 
 		ctx.closePath();
 
-		// style
-		ctx.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')';
 		ctx.shadowColor = '#FFF';
 		ctx.shadowBlur = 20;
 		ctx.shadowOffsetX = 0;
 		ctx.shadowOffsetY = 0;
 
-		ctx.fill();
+		
 	};
 }
 
@@ -113,20 +194,17 @@ var shapeCreator = {
 			var minSpeed = opts.minSpeed || 1;
 			var maxSpeed = opts.maxSpeed || 5;
 			var startingX = opts.startingX || 5;
-
-			var randomX = generateNumBetween(-200, opts.canvas.width);
-			var randomY = generateNumBetween(0, opts.canvas.height);
 			var speed = generateNumBetween(minSpeed, maxSpeed);
 			var size = generateNumBetween(0, opts.maxSize);
 
 			var shape = new Shape({
 				canvas: opts.canvas,
 				context: opts.context,
+				movement: opts.movement,
 				shape: opts.shape,
+				style: opts.style,
 				size: size,
-				speed: speed,
-				xPos: randomX,
-				yPos: randomY
+				speed: speed
 			})
 
 			set.shapes.push(shape);
@@ -136,24 +214,27 @@ var shapeCreator = {
 		this.numOfSets++;
 
 		draw();
-		console.log(this);
 	}
 };
 
 var init = function() {
 
+	// generate circles in first canvas
 	var canvas = document.getElementById('dxp-background');
 	var ctx = canvas.getContext('2d');
 
 	shapeCreator.add({
 		canvas: canvas,
 		context: ctx,
+		movement: 'converge',
 		shape: 'circle',
 		num: 30,
 		maxSpeed: 5,
-		maxSize: 10
+		maxSize: 10,
+		style: 'solid'
 	});
 
+	// generate squares
 	var canvas = document.getElementById('dxp-background2');
 	var ctx = canvas.getContext('2d');
 
@@ -161,9 +242,22 @@ var init = function() {
 		canvas: canvas,
 		context: ctx,
 		shape: 'square',
-		num: 10,
+		movement: 'expand',
+		num: 30,
 		maxSpeed: 5,
-		maxSize: 10
+		maxSize: 10,
+		style: 'outline'
+	});
+
+	shapeCreator.add({
+		canvas: canvas,
+		context: ctx,
+		shape: 'rectangle',
+		movement: 'expand',
+		num: 30,
+		maxSpeed: 5,
+		maxSize: 10,
+		style: 'outline'
 	});
 };
 
