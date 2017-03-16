32bdf30d38e269dd6683eb31deebf6b761ad0c0e
Phillip Chan <phillip.chan@liferay.com>
2017-03-15 17:33:47 -0700

    Can handle multiple shapes on a context

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-var generateNumBetween = function(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-};
+var utils = (function() {
+	return {
+		generateNumBetween: function(min, max) {
+			return Math.floor(Math.random() * (max - min + 1) + min);
+		},
+		checkIfArrayIsUnique: function(arr) {
+		    var map = {}, i, size;
+
+		    for (i = 0, size = arr.length; i < size; i++){
+		        if (map[arr[i]]){
+		            return false;
+		        }
+
+		        map[arr[i]] = true;
+		    }
+
+		    return true;
+		}
+	}
+})();
 
 var draw = function() {
 	
@@ -37,8 +54,8 @@ var Shape = function(opts) {
 	this.movements = {
 		converge: {
 			starting: function() {
-				var xPos = generateNumBetween(-200, opts.canvas.width);
-				var yPos = generateNumBetween(0, opts.canvas.height);
+				var xPos = utils.generateNumBetween(-200, opts.canvas.width);
+				var yPos = utils.generateNumBetween(0, opts.canvas.height);
 
 				self.originalY = yPos
 				self.originalX = xPos
@@ -64,8 +81,8 @@ var Shape = function(opts) {
 		},
 		expand: {
 			starting: function() {
-				var xPos = generateNumBetween(-200, opts.canvas.width / 2);
-				var yPos = generateNumBetween(
+				var xPos = utils.generateNumBetween(-200, opts.canvas.width / 2);
+				var yPos = utils.generateNumBetween(
 					// 40% from top
 					(opts.canvas.height / 2) - (opts.canvas.height * .1), 
 
@@ -108,7 +125,7 @@ var Shape = function(opts) {
 			self.context.fill();
 		},
 		outline: function() {
-			self.context.strokeStyle = 'rgba(185, 211, 238,' + self.opacity + ')';
+			self.context.strokeStyle = 'rgba(240, 241, 248,' + self.opacity + ')';
 			self.context.stroke();
 		}
 	};
@@ -178,24 +195,20 @@ var Shape = function(opts) {
 }
 
 var shapeCreator = {
-	numOfSets: 0,
 	sets: [],
 	add: function(opts) {
-		var set = {};
 
-		set.shapes = [];
-		set.context = opts.context;
-		set.canvas = opts.canvas;
-
-		if (!opts.shape) return;
+		// check if canvas is unique
+		var canvasUniqueness = this.checkSetUniqueness(opts);
+		var shapes = [];
 
 		for (var i = 0; i < opts.num; i++) {
 
 			var minSpeed = opts.minSpeed || 1;
 			var maxSpeed = opts.maxSpeed || 5;
 			var startingX = opts.startingX || 5;
-			var speed = generateNumBetween(minSpeed, maxSpeed);
-			var size = generateNumBetween(0, opts.maxSize);
+			var speed = utils.generateNumBetween(minSpeed, maxSpeed);
+			var size = utils.generateNumBetween(0, opts.maxSize);
 
 			var shape = new Shape({
 				canvas: opts.canvas,
@@ -207,13 +220,41 @@ var shapeCreator = {
 				speed: speed
 			})
 
-			set.shapes.push(shape);
+			shapes.push(shape);
+		}
+		
+		// if context already exists, add it to it
+		if (canvasUniqueness !== true) {
+			console.log(canvasUniqueness)
+			canvasUniqueness['shapes'] = canvasUniqueness['shapes'].concat(shapes);
+
+		// otherwise create a new set
+		} else {
+			var set = {};
+
+			set.shapes = shapes;
+			set.context = opts.context;
+			set.canvas = opts.canvas;
+	
+			this.sets.push(set)
+
+			draw();
+			console.log(this);
+		}
+		
+	},
+
+	// checks if a set is unique, if it is return true, otherwise return the set
+	checkSetUniqueness: function(opts) {
+		var canvasUniqueness = true;
+
+		for (var x = 0; x < this.sets.length; x++) {
+			if (this.sets[x].canvas === opts.canvas) {
+				return this.sets[x];
+			} 
 		}
 
-		this.sets.push(set)
-		this.numOfSets++;
-
-		draw();
+		return canvasUniqueness;
 	}
 };
 
@@ -228,7 +269,7 @@ var init = function() {
 		context: ctx,
 		movement: 'converge',
 		shape: 'circle',
-		num: 30,
+		num: 20,
 		maxSpeed: 5,
 		maxSize: 10,
 		style: 'solid'
@@ -243,6 +284,17 @@ var init = function() {
 		context: ctx,
 		shape: 'square',
 		movement: 'expand',
+		num: 20,
+		maxSpeed: 5,
+		maxSize: 10,
+		style: 'outline'
+	});
+
+	shapeCreator.add({
+		canvas: canvas,
+		context: ctx,
+		shape: 'circle',
+		movement: 'expand',
 		num: 30,
 		maxSpeed: 5,
 		maxSize: 10,
@@ -254,7 +306,7 @@ var init = function() {
 		context: ctx,
 		shape: 'rectangle',
 		movement: 'expand',
-		num: 30,
+		num: 10,
 		maxSpeed: 5,
 		maxSize: 10,
 		style: 'outline'
